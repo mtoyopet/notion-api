5eff561ac6bd2844a3503f74b0f2088562fb4adf
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-08-31 16:33:42 +0900

    port changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const addItem = require('./notion.js')
 
 app.get('/notion', (req, res) => {
